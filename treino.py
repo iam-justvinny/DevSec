a2b5a260fbacf039a58388b40b2c63b10742c16d
TiagoAM-tech <tiago.mirandacontato@gmail.com>
2024-09-30 18:37:48 -0300

    treino

--- treino.py
+++ treino.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um número:'))
 n2 = int(input('Digite mais um número:'))
 s = n1+n2
-print('A soma de {} e {}, dá {}' .format(n1, n2, s))
+print(f'A soma de {n1} e {n2}, dá {s}')
