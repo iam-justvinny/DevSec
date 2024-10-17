799446399694c85596c6195fb499f23a6655d541
iam-justvinny <vinicius.dev08@gmail.com>
2024-10-17 12:47:49 -0300

    aula48 udemy

---
+++ Vinícius/aula48.py
@@ -0,0 +1,14 @@
+'''
+Listas em Python
+Tipos list - Muável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizávis - índices e atiamento
+Métodos úteis: append insert, pop, del, clear, extend, +
+'''
+#        +01234
+#        -54321
+string = 'ABCDE'  # 5 caracteres (len)
+# print(bool([])) # false
+# print(lista, type(lista))
+lista = [123, True, 'vinicus', 1.3, []]  # ''
+print(lista)
