579b10c5bf03b7b9035429bb3ee435b77e6a73e9
iam-justvinny <vinicius.dev08@gmail.com>
2024-10-17 23:01:53 -0300

    finalização do cadastro

--- Vinícius/cadastro.py
+++ Vinícius/cadastro.py
@@ -1,4 +1,6 @@
-
+print('-' * 100)
+print('Olá! Seja bem vindo(a) ao cadastro de currículo.')
+print('-' * 100)
 # Captura do número de usuários que serão cadastrados
 n = int(input("Quantos usuários deseja cadastrar? "))
 
@@ -10,7 +12,31 @@ for i in range(n):
     print(f"\n--- Cadastro do Usuário {i + 1} ---")
     nome = input("Nome: ")
     cpf = int(input("CPF: "))
-    estado_civil = input("Estado Civil: ")
+    # Laço para garantir a escolha de uma opção válida de estado civil
+    while True:
+        print("\nEscolha o estado civil:")
+        print("[1] Solteiro")
+        print("[2] Casado")
+        print("[3] Divorciado")
+        print("[4] Viúvo")
+        opcao = int(input("Opção: "))
+
+        # Verifica se a opção é válida e armazena o estado civil correspondente
+        if opcao == '1':
+            estado_civil = "Solteiro(a)"
+            break
+        elif opcao == '2':
+            estado_civil = "Casado(a)"
+            break
+        elif opcao == '3':
+            estado_civil = "Divorciado(a)"
+            break
+        elif opcao == '4':
+            estado_civil = "Viúvo(a)"
+            break
+        else:
+            print("Opção inválida! Tente novamente.")
+
     resumo_curriculo = input("Resumo do Currículo: ")
 
     # Adiciona os dados do usuário como uma nova linha na matriz
