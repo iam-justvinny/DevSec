7757eeaf466a3c524a401c32178cfe3c3962cc68
iam-justvinny <vinicius.dev08@gmail.com>
2024-10-18 23:37:01 -0300

    teste vinny

--- Vinícius/cadastro.py
+++ Vinícius/cadastro.py
@@ -3,12 +3,21 @@ print('Olá, seja bem vindo ao cadastro de currículo!')
 print('-' * 100)
 nome = input('Para começar digite seu nome\nNome:  ')
 cpf = int(input(f'{nome}, digite seu cpf\nCPF: '))
-estado_civil = False
-while estado_civil == True:
-    estado_civil = int(input(f'\n{nome}, qual seu estado civil? \n[1]Solteiro(a) \n[2]Casado(a) \n[3]Divorciado(a) \n[4]Viúvo(a)\nEstado civil: '))
-    if '1' or '2' or '3' or '4' not in estado_civil:
-        estado_civil = False
+
+while True:
+    estado_civil = input(f'\n{nome}, qual seu estado civil? \n[1]Solteiro(a) \n[2]Casado(a) \n[3]Divorciado(a)\n[4]Viúvo(a)\nEstado civil: ')
+    if estado_civil == 1:
+        estado_civil = 'Solteiro(a)'
+        break
+    elif estado_civil == 2:
+        estado_civil = 'Casado(a)'
+        break
+    elif estado_civil == 3:
+        estado_civil = 'Divorciado(a)'
+        break
+    elif estado_civil == 4:
+        estado_civil = 'Viúvo(a)'
+        break
     else:
-        estado_civil = True
-    break
+        print('Opção incorreta')
 print("acabou")
