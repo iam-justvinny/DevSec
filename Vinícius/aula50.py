0491c46987c6e3b8c060cada506ab44c1eeda7e1
iam-justvinny <vinicius.dev08@gmail.com>
2024-11-14 12:12:49 -0300

    commit

--- Vinícius/aula50.py
+++ Vinícius/aula50.py
@@ -4,8 +4,11 @@ Exiba os índices da lista
 0 Maria
 1 Helena
 3 Luiza
-'''
+''' 
 lista = ['Maria', 'Helena', 'Luiza']
+lista.append('jõao')
 
-for nome in lista:
-    print(nome, type(nome))
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
---
+++ aula56.py
@@ -0,0 +1,8 @@
+'''
+Introdução às funções (def) em Python
+Funções são trechos d ecódigo usados para
+replicar determinada ação ao lingo do seu código.
+Elas podem receber valores para parâmetros (argumentos)
+e retornar um valor específico.
+Por padrão, funções Python retornam None (nada).
+'''
