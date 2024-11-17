e68a7222a59513c4b003ebee13b45a4af36ff282
iam-justvinny <vinicius.dev08@gmail.com>
2024-11-17 20:11:02 -0300

    aula 70 return

---
+++ Vinícius/aula70.py
@@ -0,0 +1,13 @@
+'''
+Retorno de valores das funções (return)
+'''
+def soma(x, y):
+    return x + y
+
+
+#var = soma(1, 2)
+#var = int('1')
+soma1 = soma(2, 2)
+soma2 = soma(3, 3)
+print(soma1 + soma2)
+print(var)
