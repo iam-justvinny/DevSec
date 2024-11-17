220b25e932f836f7cab142aa2d6341730e6a670d
iam-justvinny <vinicius.dev08@gmail.com>
2024-11-17 18:41:17 -0300

    aula def 66

--- Vinícius/aula66.py
+++ Vinícius/aula66.py
@@ -7,6 +7,7 @@ Argumentos não nomeados recebem apenas o argumento(valor)
 
 def soma(x, y): 
     #definição
-    print(f'{x=} + y', x + y)
+    print(f'{x=} y={y}', '|', 'x + y = ', x + y)
 
 soma(1,2)
+soma(y=2, x=1)
