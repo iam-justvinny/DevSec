7e514e8c63ded7d23aa95b75d0bea69e1701fd81
iam-justvinny <vinicius.dev08@gmail.com>
2024-11-17 18:24:40 -0300

    AULA SOBRE DEF

---
+++ Vinícius/aula66.py
@@ -0,0 +1,12 @@
+'''
+Argumentos nomeados e não nomeados em funções python
+Argumentos nomeados tem nome com sinal de igual
+Argumentos não nomeados recebem apenas o argumento(valor)
+'''
+
+
+def soma(x, y): 
+    #definição
+    print(f'{x=} + y', x + y)
+
+soma(1,2)
