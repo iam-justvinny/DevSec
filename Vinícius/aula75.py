bad03b265ee74ea172b1b01f96de85a0c396f013
iam-justvinny <vinicius.dev08@gmail.com>
2024-11-17 23:50:53 -0300

    aula 75 multiplicador

---
+++ Vinícius/aula75.py
@@ -0,0 +1,18 @@
+#def triplicar(numero):
+#   return numero * 3
+
+
+# def quadruplicar(numero):
+#    return numero * 4
+def criar_multiplicador(multiplicador):
+    def multiplicar(numero):
+        return numero * multiplicador
+    return multiplicar
+
+
+duplicar = criar_multiplicador(2)
+triplicar = criar_multiplicador(3)
+quadruplicar = criar_multiplicador(4)
+print(duplicar(2))
+print(triplicar(3))
+print(quadruplicar(9))
